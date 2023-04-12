8bfa93df67917d79b053e70c37497e0d4cf01849
khalids01 <khalids8774@gmail.com>
2023-04-13 00:14:30 +0600

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import SocketsProvider from '@/context/socket.context'
+import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
+
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    setShow(true)
+  }, [])
+
+  if (!show) {
+    return <></>
+  }
+
+  return (
+    <SocketsProvider>
+      <MantineProvider theme={{ colorScheme: 'dark', fontFamily: inter.style.fontFamily }} withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </SocketsProvider>
+  )
 }
